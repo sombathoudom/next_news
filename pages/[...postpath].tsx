54ad2ac2418f2c3973cbb4d4d57e95aa4f45a6a4
Nour Rachana <rachananour@gmail.com>
2023-02-09 17:21:27 +0700

    update

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -82,9 +82,9 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={post.title} />
+				<meta property="og:title" content={"."} />
 				<link rel="canonical" href={`https://${host}/${path}`} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
+				<meta property="og:description" content={' '} />
 				<meta property="og:url" content={`https://${host}/${path}`} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
