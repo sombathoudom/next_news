5120c1786d5f8c2708e690335100318d191be062
Chheng Sombathoudom <sombathoudom001@example.com>
2023-02-13 00:26:00 +0700

    w

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -82,7 +82,7 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={undefined}/>
+				<meta property="og:title" content={'undefined'}/>
 				<link rel="canonical" href={`https://${host}/${path}`} />
 				<meta property="og:description" content= " " />
 				<meta property="og:url" content={" "} />
